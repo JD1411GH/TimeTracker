4bff76e52cf0b2553b0757575b773fa76d8632a9
GigabyteLinux <jayanta.dn@gmail.com>
2022-12-29 22:58:39 +0530

    scrolling text

--- gui.py
+++ gui.py
@@ -5,13 +5,15 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 
+from datetime import *
+
 
 class MainLayout(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.cols = 1
 
-        textinput = TextInput(text='Hello world')
+        textinput = TextInput()
         self.add_widget(textinput)
 
         layout_button = GridLayout(cols=2)
@@ -22,9 +24,16 @@ class MainLayout(GridLayout):
         layout_button.add_widget(button_start)
         layout_button.add_widget(button_stop)
 
+        def func(instance):
+            textinput.text += f"{datetime.now()}: Hello World\n"
+
+        button_start.bind(on_press=func)
+
+
 class MainApp(App):
     def build(self):
         return MainLayout()
+
 
 class Gui:
     def __init__(self) -> None:
