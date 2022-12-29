d09aa52a9d7b122dcba3d604fd5864c0f89a0940
GigabyteLinux <jayanta.dn@gmail.com>
2022-12-29 22:10:02 +0530

    layout complete

--- gui.py
+++ gui.py
@@ -3,14 +3,24 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
 
 class MainLayout(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.cols = 1
-        self.add_widget(Label(text="Name: "))
 
+        textinput = TextInput(text='Hello world')
+        self.add_widget(textinput)
+
+        layout_button = GridLayout(cols=2)
+        self.add_widget(layout_button)
+
+        button_start = Button(text='Start')
+        button_stop = Button(text='Stop')
+        layout_button.add_widget(button_start)
+        layout_button.add_widget(button_stop)
 
 class MainApp(App):
     def build(self):
