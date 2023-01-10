92d805160756f8ce0cd6d0fb458dd15eebb29565
Jayanta Debnath <Jayanta.Debnath@de.bosch.com>
2023-01-10 11:17:43 +0530

    bugfix thread

--- sedtimer.py
+++ sedtimer.py
@@ -13,27 +13,29 @@ config.read(configfile)
 
 class SedTimer:
     def __init__(self, handler) -> None:
+        self.flg_cancel = False
+        self.flg_stop = True
         self.th_sed = Thread(target=self._th_timer, args=[
                              handler, ], daemon=True)
-        self.flg_cancel = False
-        self.flg_stop = False
+        self.th_sed.start()
 
     def _th_timer(self, handler):
         init = int(config['DEFAULT']['SED_TIMER'])
         eta = timedelta(minutes=init)
-        while not self.flg_stop:
+        while not self.flg_cancel:
             time.sleep(1)
             eta -= timedelta(seconds=1)
-            if eta.total_seconds() <= 0:
-                handler("Time to take a walk")
-                eta = timedelta(minutes=init)
+            if not self.flg_stop:
+                if eta.total_seconds() <= 0:
+                    handler("Time to take a walk")
+                    eta = timedelta(minutes=init)
 
     def is_running(self):
-        return self.th_sed.is_alive()
+        return not self.flg_stop
 
     def start(self):
-        if not self.flg_cancel:
-            self.th_sed.start()
+        if not self.is_running() or not self.flg_cancel:
+            self.flg_stop = False
 
     def stop(self):
         if self.is_running():
